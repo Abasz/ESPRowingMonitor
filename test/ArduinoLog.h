a4e166989695a32488536cbf7d1cf888045c2d42
Abász <>
2023-11-25 17:13:41 +0100

    Add log levels to test runs
    Make logging adhere the log levels when doing test runs and creating
    outputs.

--- test/ArduinoLog.h
+++ test/ArduinoLog.h
@@ -1,6 +1,7 @@
 // NOLINTBEGIN
 #pragma once
 
+#include "../src/utils/configuration.h"
 #include <stdio.h>
 
 class Logging
@@ -9,36 +10,51 @@ public:
     template <class T, typename... Args>
     static void infoln(T msg, Args... args)
     {
-        printf(msg, args...);
-        printf("\n");
+        if (Configurations::defaultLogLevel >= ArduinoLogLevel::LogLevelInfo)
+        {
+            printf(msg, args...);
+            printf("\n");
+        }
     };
 
     template <class T, typename... Args>
     static void verboseln(T msg, Args... args)
     {
-        printf(msg, args...);
-        printf("\n");
+        if (Configurations::defaultLogLevel >= ArduinoLogLevel::LogLevelVerbose)
+        {
+            printf(msg, args...);
+            printf("\n");
+        }
     };
 
     template <class T, typename... Args>
     static void traceln(T msg, Args... args)
     {
-        printf(msg, args...);
-        printf("\n");
+        if (Configurations::defaultLogLevel >= ArduinoLogLevel::LogLevelTrace)
+        {
+            printf(msg, args...);
+            printf("\n");
+        }
     };
 
     template <class T, typename... Args>
     static void warningln(T msg, Args... args)
     {
-        printf(msg, args...);
-        printf("\n");
+        if (Configurations::defaultLogLevel >= ArduinoLogLevel::LogLevelWarning)
+        {
+            printf(msg, args...);
+            printf("\n");
+        }
     };
 
     template <class T, typename... Args>
     static void errorln(T msg, Args... args)
     {
-        printf(msg, args...);
-        printf("\n");
+        if (Configurations::defaultLogLevel >= ArduinoLogLevel::LogLevelError)
+        {
+            printf(msg, args...);
+            printf("\n");
+        }
     };
 
     inline void setLevel(int level) {}
